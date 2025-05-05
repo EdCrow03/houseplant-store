f12d9136a92bf3ad7956501ac80536d23d83c2b0
Salim Ahouad <ahouadsalim@gmail.com>
2025-05-05 16:07:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import Header from './components/Header';
+import LandingPage from './pages/LandingPage';
+import ProductListing from './pages/ProductListing';
+import ShoppingCart from './pages/ShoppingCart';
+import './App.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/products" element={<ProductListing />} />
+            <Route path="/cart" element={<ShoppingCart />} />
+          </Routes>
+        </main>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
